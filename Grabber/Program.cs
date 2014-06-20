7d7e30e05245f75a746c1097faf3411dfbc383ec
Katsiaryna-Ledak <Katsiaryna_Ledak@epam.com>
2014-06-20 16:37:22 +0300

    Grabber

--- Grabber/Program.cs
+++ Grabber/Program.cs
@@ -1,66 +1,107 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.IO.Compression;
 using System.Text.RegularExpressions;
 using System.Net;
+
 
 
 namespace Grabber
 {
     class Program
     {
+        static int p = 0;
         static void Main(string[] args)
         {
-            UnicodeEncoding uniEncode = new UnicodeEncoding();
-            // Адрес страницы без индекса фильма
-            string url = "http://www.imdb.com/title/tt";
-            // индекс названия фильма
-            string movieName = "0814314";
-            // первая страница отзывов
-            string page1 = "0";
+            int kol = 0; //количество страниц в архиве
+            int index = 50; //количество страниц с отзывами об одном фильме
 
-            // Формируем адрес сайта
-            // http://www.imdb.com/title/tt0814314/reviews?start=0
-            string newUrl = url + movieName + "/reviews?start=" + page1;
-            
-            // Отправляем GET запрос и получаем в ответ HTML-код сайта
-            HttpWebRequest myHttpWebRequest = (HttpWebRequest)HttpWebRequest.Create(newUrl);
-            myHttpWebRequest.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip");
-            HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
-            StreamReader myStreamReader = new StreamReader(myHttpWebResponse.GetResponseStream());
+            GetIndexFromReviewPage();
 
-            string path = @"C:\Users\Katsiaryna_Ledak\Documents\Visual Studio 2013\Projects\Grabber\AllMovies.gz";
-            FileStream file = File.OpenWrite(path);
-            StreamWriter myStreamWriter = new StreamWriter(file);
-            myStreamWriter.Write(myStreamReader.ReadToEnd());
-            
-            /*byte[] bytesToCompress = uniEncode.GetBytes(myStreamReader.ReadToEnd());
-            string path = @"C:\Users\Katsiaryna_Ledak\Documents\Visual Studio 2013\Projects\Grabber\AllMovies.gz";
-            using (FileStream fileToCompress = File.Create(path))
+            //цикл по всем страницам в одном фильме
+            for (int pageNumber = 0; pageNumber < index; pageNumber = pageNumber + 10)
             {
-                using (GZipStream compressionStream = new GZipStream(fileToCompress, CompressionMode.Compress))
-                {
-                    compressionStream.Write(bytesToCompress, 0, bytesToCompress.Length);
-                }
-            }
+                // Отправляем GET запрос и получаем в ответ HTML-код сайта
+                string str = GetMovieNumber();
+                HttpWebRequest myHttpWebRequest = (HttpWebRequest)HttpWebRequest.Create(MakeURL(str, pageNumber));
+                myHttpWebRequest.KeepAlive = false;
+                myHttpWebRequest.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB; rv:1.9.2.4) Gecko/20100611 Firefox/3.6.4";
+                myHttpWebRequest.Method = "GET";
+                myHttpWebRequest.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip");
+                HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
 
-            byte[] decompressedBytes = new byte[bytesToCompress.Length];
-            using (FileStream fileToDecompress = File.Open(path, FileMode.Open))
-            {
-                using (GZipStream decompressionStream = new GZipStream(file, CompressionMode.Decompress))
+                //формируем название архива
+                string path = @"D:\movie = " + str + "review" + pageNumber + ".gz";
+
+                using (FileStream file = File.OpenWrite(path))
                 {
-                    decompressionStream.Read(decompressedBytes, 0, bytesToCompress.Length);
+                    myHttpWebResponse.GetResponseStream().CopyTo(file);
                 }
+                Console.WriteLine("{0}", path);
+                kol++;
             }
-            */
-            myStreamReader.Close();
-            myStreamWriter.Close();
+            Console.WriteLine("number of pages = {0}", kol);
+            /* когда kol = 100 (например), засунуть их в большой архив*/
+
+
+            string directoryPath = @"D:\AllReviews0";
+            DirectoryInfo directorySelected = new DirectoryInfo(directoryPath);
 
             Console.ReadLine();
         }
+
+        //получить ссылку на страницу с отзывом у конкретного фильма
+        public static string MakeURL (string movieNumber, int pageNum)
+        {
+            string url = "http://www.imdb.com/title/tt";  // Адрес страницы без индекса фильма
+            string newUrl = url + movieNumber + "/reviews?start=" + pageNum.ToString();
+            return newUrl;
+        }
+
+        //вычислить номер фильма
+        public static string GetMovieNumber()
+        {
+            string num = "000000";
+            p++;
+            return num + p.ToString();
+        }
+
+        //получить первую страницу с отзывами в .html
+        public static string GetFirstPageOfMovie()
+        {
+            string mn = GetMovieNumber();
+            string url = "http://www.imdb.com/title/tt" + mn + "/reviews?start=0";
+
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+            myHttpWebRequest.KeepAlive = false;
+            myHttpWebRequest.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB; rv:1.9.2.4) Gecko/20100611 Firefox/3.6.4";
+            myHttpWebRequest.Method = "GET";
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+
+            string path = @"D:\movie = " + mn + "page0.html";
+
+            using (FileStream file = File.OpenWrite(path))
+            {
+                myHttpWebResponse.GetResponseStream().CopyTo(file);
+            }
+            return path;
+        }
+
+        public static void GetIndexFromReviewPage()
+        {
+            GetFirstPageOfMovie();
+            string html = File.ReadAllText(GetFirstPageOfMovie());
+
+            string sPattern = "Index [0-9] reviews in total";
+
+            Regex r = new Regex(sPattern, RegexOptions.IgnoreCase);
+            Match m = r.Match(html);
+            /*искать совпадения и вытаскивать оттуда число index*/
+        }
+
     }
 }
